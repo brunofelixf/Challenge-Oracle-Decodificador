259abb38d2d6e67a988188c250d703eac4334a0f
brunofelixf <brunofelixf@gmail.com>
2022-08-19 02:38:15 -0300

    feat: captura botão descriptografar e chama a função

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.querySelector('.criptografar').addEventListener('click',()=>{cript()})
+document.querySelector('.descriptografar').addEventListener('click',()=>{descript()})
 
 function cript() {
   
@@ -39,4 +40,45 @@ function cript() {
 
   let result = document.querySelector('.texto__criptografado')
   result.innerHTML = resultado
+}
+
+function descript() {
+  
+  // document.querySelector('.blc__img').style.display = 'none';
+  // document.querySelector('.btn__copiar').style.visibility = 'visible';
+
+  // const msg = document.getElementById( 'mensagem');
+
+  // let texto = msg.value
+  // let char = texto.split('');
+  // var resultado ='';
+  
+  // char.forEach(c => {
+  //   switch (c) {
+  //     case 'e':
+  //       resultado += 'enter';
+  //       break;
+  //     case 'a':
+  //       resultado += 'ai';
+  //       break;
+  //     case 'i':
+  //       resultado += 'imes';
+  //       break;
+  //     case 'o':
+  //       resultado += 'ober';
+  //       break;
+  //     case 'u':
+  //       resultado += 'ufat';
+  //       break;
+    
+  //     default:
+  //       resultado += c
+  //       break;
+  //   }
+  // });
+
+  // console.log(resultado);
+
+  // let result = document.querySelector('.texto__criptografado')
+  // result.innerHTML = resultado
 }
